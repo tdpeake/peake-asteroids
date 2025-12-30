adbb6401c68ff81ce7040b6a130553918dc49e54
tdpeake <tdpeake@gmail.com>
2025-12-30 11:21:13 -0500

    Setup Display to show on screen

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import *
+from logger import log_state
+
+def main():
+    print(f"Starting Asteroids withpygame version: {pygame.version.ver}\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
