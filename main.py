a866d652666f5a5bd7e0e2c9db8b72b17945c6f7
tdpeake <tdpeake@gmail.com>
2025-12-30 12:11:51 -0500

    Draw and Move Player using pygame groups

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
-    print(f"Starting Asteroids withpygame version: {pygame.version.ver}\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
+        dt = game_clock.tick(60)/1000
+        
 
 
 if __name__ == "__main__":
